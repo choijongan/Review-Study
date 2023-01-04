36d349beafe4d505c308927b8ce21371720fde4a
choijongan <mmm7134@naver.com>
2023-01-04 20:56:52 +0900

    가위바위보 완성

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -26,13 +26,28 @@ const choice = {
 function App() {
   const [userSelect, setUserSelect] = useState(null)
   const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState('')
   const play = (userChoice) => {
     setUserSelect(choice[userChoice])
     let computerChoice = randomChoice()
     setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice], computerChoice))
+  };
+
+  const judgement = (user,computer) =>{
+    console.log('user',user,'computer',computer)
+
+    if(user.name == computer.name) {
+      return 'tie';
+    }else if(user.name =='Rock')
+      return computer.name =='Scissors' ? 'win':'lose';
+    else if(user.name =='Scissors')
+      return computer.name =='Paper' ? 'win':'lose';
+    else if(user.name =='Paper')
+      return computer.name =='Rock' ? 'win':'lose';
   }
 
-  const randomChoice=()=>{
+  const randomChoice = () =>{
     let itemArray = Object.keys(choice) //객체에 키값만 뽑아서 어래이로 만들어주는 함수.
     console.log('item array',itemArray)
     let randomItem = Math.floor(Math.random()*itemArray.length); //floor 소수점 버리기
@@ -42,8 +57,8 @@ function App() {
   return (
   <div>  
     <div className='main'>
-      <Box title='You' item={userSelect}/>
-      <Box title='Computer' item={computerSelect}/>
+      <Box title='You' item={userSelect} result={result}/>
+      <Box title='Computer' item={computerSelect} result={result}/>
     </div> 
     <div className='main'>
       <button onClick={() => play('scissors')}>가위</button> 
