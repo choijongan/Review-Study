99c0e44c0ea3bcd662b3353c9ba175b66d332cec
choijongan <mmm7134@naver.com>
2023-01-27 23:26:14 +0900

    Restful Route로 페이지이동

--- router-study/src/App.js
+++ router-study/src/App.js
@@ -2,6 +2,8 @@ import Homepage from './page/Homepage';
 import AboutPage from './page/AboutPage';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
+import ProductPage from './page/ProductPage';
+import ProductDetailPage from './page/ProductDetailPage';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
      <Routes>
         <Route path='/' element={<Homepage />}/>
         <Route path='/about' element={<AboutPage />}/>
+        <Route path='/products' element={<ProductPage />}/>
+        <Route path='/products/:id' element={<ProductDetailPage />}/>
      </Routes> 
     </div>
   );
---
+++ router-study/src/page/ProductDetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetailPage = () => {
+  return (
+    <div>
+        <h1>Show product detail</h1>
+    </div>
+  )
+}
+
+export default ProductDetailPage
