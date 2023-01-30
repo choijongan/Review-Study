e07f6b42a971c464787c7a1a4dbb1b544448ed06
choijongan <mmm7134@naver.com>
2023-01-30 17:56:17 +0900

    Navigate로 리다이렉트해서 페이지 보호

--- router-study/src/App.js
+++ router-study/src/App.js
@@ -1,11 +1,18 @@
 import Homepage from './page/Homepage';
 import AboutPage from './page/AboutPage';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
+import LoginPage from './page/LoginPage';
+import { useState } from 'react';
+import UserPage from './page/UserPage';
 
 function App() {
+  const [authenticate, setAuthenticate]=useState(false)
+  const PrivateRoute = () => {
+      return authenticate == true?<UserPage/>:<Navigate to='/login'/> //Navigate:컴포넌트, 리다이렉트 도와줌
+  }
   return (
     <div>
      <Routes>
@@ -13,6 +20,8 @@ function App() {
         <Route path='/about' element={<AboutPage />}/>
         <Route path='/products' element={<ProductPage />}/>
         <Route path='/products/:id' element={<ProductDetailPage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/user' element={<PrivateRoute />}/>
      </Routes> 
     </div>
   );
