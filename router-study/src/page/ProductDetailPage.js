b2a809525dd9e7a837e127a50487fa302bc43076
choijongan <mmm7134@naver.com>
2023-01-30 16:52:08 +0900

    useParams로 URL 파라미터값 읽어오기

--- router-study/src/page/ProductDetailPage.js
+++ router-study/src/page/ProductDetailPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductDetailPage = () => {
+  const {id} = useParams()
+  
   return (
     <div>
-        <h1>Show product detail</h1>
+        <h1>Show product detail{id}</h1>
     </div>
   )
 }
