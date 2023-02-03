45fb2919c8b4c756a3c75f7b38e395eaa08e6b2e
choijongan <mmm7134@naver.com>
2023-02-03 21:23:51 +0900

    API 불러오기

---
+++ hnm-shoppingmall/src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://lp2.hm.com/hmgoepprod?set=source[/d6/f1/d6f154343c60dca421fa099ea82977bbbdf90e8f.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]'/>
+        <div>Consciou choice</div>
+        <div>상품이름</div>
+        <div>가격</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
