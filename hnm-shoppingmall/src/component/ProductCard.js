897f5fce0616dfe8cc1b0351dc0ebc9982f38313
choijongan <mmm7134@naver.com>
2023-02-06 17:36:36 +0900

    부트스트랩 사용하여 메인 꾸미기

--- hnm-shoppingmall/src/component/ProductCard.js
+++ hnm-shoppingmall/src/component/ProductCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({item}) => {
   return (
     <div>
-        <img src='https://lp2.hm.com/hmgoepprod?set=source[/d6/f1/d6f154343c60dca421fa099ea82977bbbdf90e8f.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]'/>
-        <div>Consciou choice</div>
-        <div>상품이름</div>
-        <div>가격</div>
-        <div>신제품</div>
+        <img src={item?.img}/>
+        <div>{item?.choice ==true?'concious choie':''}</div>
+        <div>{item?.title}</div>
+        <div>{item?.price}</div>
+        <div>{item?.new ==true?'신제품':''}</div>
     </div>
   )
 }
--- hnm-shoppingmall/src/page/ProductAll.js
+++ hnm-shoppingmall/src/page/ProductAll.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
 import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
-    const [productList, setProductList] = useState([])
-    const getProducts = async()=>{
+    const [productList, setProductList] = useState([]) //UI에 데이터를 보여준다
+    const getProducts = async()=>{ //await은 async를 줘야한다.
       let url= `http://localhost:5000/products`;
-      let response = await fetch(url);
+      let response = await fetch(url); 
       let data = await response.json();
       setProductList(data)
+      //console.log(data)
     }
     useEffect(()=>{
       getProducts()
     },[]);
   return (
     <div>
+         <Container>
+            <Row>
+              {productList.map(menu=> <Col lg={3}>
+                <ProductCard item={menu} />
+              </Col>)}
+            </Row>
+        </Container>
         <ProductCard />
     </div>
   )
