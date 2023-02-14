86a6256ec75f479fd65326e98aab9c5fbbe03153
choijongan <mmm7134@naver.com>
2023-02-14 20:37:55 +0900

    상품 검색하기 1

--- hnm-shoppingmall/src/component/Navbar.js
+++ hnm-shoppingmall/src/component/Navbar.js
@@ -20,6 +20,17 @@ const Navbar = () => {
   const goToLogin=()=>{
     navigate('/login');
   };
+
+  const search = (event) => {
+    if(event.key === 'Enter') { //Enter 대소문자 구분해야됨.
+        // 입력한 검색어를 읽어와서
+        //input안의 값은 그냥 value가 아니고 event.target.value을 써줘야됨. 
+       let keyword = event.target.value 
+       
+       // url을 바꿔준다.
+       navigate(`/?q=${keyword}`) //파라미터x 서치하는 추가적인 조건에 대해선 쿼리
+    }
+  }
   return (
     <div>
         <div>
@@ -40,7 +51,7 @@ const Navbar = () => {
                 </ul>  
             <div className='search-box'>
                 <FontAwesomeIcon icon={faSearch} />
-                <input type='text' />
+                <input type='text' onKeyUp={(event)=>search(event)}/>
             </div>
         </div>
     </div>
