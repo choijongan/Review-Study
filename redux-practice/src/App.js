53af72ae5d92d30b2f3f2ccc23147bee51719d52
choijongan <mmm7134@naver.com>
2023-02-28 21:22:39 +0900

    redux로 login 만들어보기

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -4,18 +4,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import Box from './component/Box';
 
 function App() {
-
-  const count = useSelector(state=>state.count) //state를 매개변수로 줌.
-  const dispatch = useDispatch()
+  
+  let count = useSelector((state) => state.count) //state를 매개변수로 줌.
+  let id = useSelector((state)=>state.id)
+  let password = useSelector((state)=>state.password)
+  let dispatch = useDispatch()
   
   const increase=()=>{
-      dispatch({type:'INCREMENT'})  //액션은 단순한 객체, 하지만 반드시 type이라는 키와 payload라는 키 있어야됨(payload는 선택사항) ''안에 아무 이름이나 주면됨. INCREMENT(증가)임의로줌
-      
+      dispatch({type:'INCREMENT',payload:{num:5}})  //액션은 단순한 객체, 하지만 반드시 type이라는 키와 payload라는 키 있어야됨(payload는 선택사항) ''안에 아무 이름이나 주면됨. INCREMENT(증가)임의로줌
+                                //payload는 함수의 매개변수 느낌. 내가 원하는 정보를 보낼 수 있음. 
   }
+  const login=()=>{
+      dispatch({type:'LOGIN',payload:{id:'jongan',password:'123'} })
+  }
+
   return (
     <div>
+      <h1>{id},{password}</h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가!</button>
+      <button onClick={login}>Login</button>
       <Box/>
     </div>
   );
--- redux-practice/src/redux/reducer/reducer.js
+++ redux-practice/src/redux/reducer/reducer.js
@@ -1,23 +1,28 @@
 let initialState={
-    count:0
+    count:0,
+    id:'',
+    password:''
 }
 
 //reducer는 자동으로 usedispatch가 던진 액션을 받아올 수 있음.
 //store는 가만히 있다가 reducer가 return해주면 그냥 적용해줌. reducer는 항상 return해줘야됨.
 //...state는 만약 state가 여러개면 state값은 유지하되, count만 바꾼다는 뜻.
 //...state가 햇갈린다면 ...state는 기본적으로 그냥 쳐야한다고 생각하면 됨.
-function reducer(state=initialState, action){
-    if(action.type==="INCREMENT") {
-        return {...state, count: state.count + 1 } //...state치고 뒤에 바꾸고 싶은 내용
-    }
+function reducer(state=initialState, action) {
+    console.log('action은 뭘까?',action)
+    // if(action.type==="INCREMENT") {
+    //     return {...state, count: state.count + 1 } //...state치고 뒤에 바꾸고 싶은 내용
+    
     switch(action.type){ //if써도 되고, switch문 써도됨. 회사마다 쓰는걸로 쓰면됨.
         case "INCREMENT":
-            return {...state, count: state.count + 1 }
+            return {...state, count: state.count + action.payload.num }
+        case 'LOGIN':
+            return {...state, id:action.payload.id, password:action.payload.password}    
             default: 
             return {...state}
     }
-
-    return {...state} //if문 말고 기본 return도 필요
 }
+    // return {...state} //if문 말고 기본 return도 필요(이 return은 if문 return)
+
 
 export default reducer
