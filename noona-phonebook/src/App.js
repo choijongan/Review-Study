e162eeb63305299bf6eafc8d9066b568b46780bf
choijongan <mmm7134@naver.com>
2023-03-02 23:06:37 +0900

    연락처 왼쪽 UI디자인

--- noona-phonebook/src/App.js
+++ noona-phonebook/src/App.js
@@ -1,4 +1,7 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
+import ContactForm from './component/ContactForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search창이 있다.
 //2. 리스트에 유저 이름과 전화번호를 추가할 수 있다.
@@ -7,7 +10,16 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <h1 className='title'>연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
