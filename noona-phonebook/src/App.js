e7fca36f3256f535065a656880d6ea9bac7ca886
choijongan <mmm7134@naver.com>
2023-03-03 22:17:13 +0900

    오른쪽 UI 디자인중

--- noona-phonebook/src/App.js
+++ noona-phonebook/src/App.js
@@ -2,6 +2,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
 import ContactForm from './component/ContactForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactList from './component/ContactList';
 
 //1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search창이 있다.
 //2. 리스트에 유저 이름과 전화번호를 추가할 수 있다.
@@ -17,6 +18,7 @@ function App() {
             <ContactForm />
           </Col>
           <Col>
+            <ContactList />
           </Col>
         </Row>
       </Container>
