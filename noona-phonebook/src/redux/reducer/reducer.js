cbac37cc81a245760778df686289af16cbd1c5e8
choijongan <mmm7134@naver.com>
2023-03-06 20:50:05 +0900

    리덕스 기본세팅

---
+++ noona-phonebook/src/redux/reducer/reducer.js
@@ -0,0 +1,7 @@
+let initialState={} //기본적인 세팅값이 없을 때는 공백으로 둔다.
+
+function reducer(state=initialState,action){ //state는 항상 처음에 initialState를 넣어줘서 초기화 해야됨
+
+}
+
+export default reducer
--- noona-phonebook/src/redux/store.js
+++ noona-phonebook/src/redux/store.js
@@ -1,3 +1,6 @@
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
+import reducer from "./reducer/reducer";
 
 let store = createStore(reducer)
+
+export default store
