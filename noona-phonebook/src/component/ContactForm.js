b23777ac8034dbb49c9851b691a22f297767d5c1
choijongan <mmm7134@naver.com>
2023-03-08 19:34:41 +0900

    연락처 추가

--- noona-phonebook/src/component/ContactForm.js
+++ noona-phonebook/src/component/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useDispatch } from 'react-redux';
 
 const ContactForm = () => {
   const [name,setName]=useState('')
@@ -9,10 +10,17 @@ const ContactForm = () => {
   //                        event.target.value로 form에 있는 정보를 받아볼 수 있다.
   // onChange쪽 getName하나 때문에 const가 많아져서 함수 만들어 주지 않고 그 자리에 함수(setName)을 바로 줘버림.
   // onChange={}라는 event는 입력할 때마다 타입한 값을 읽어와서 저장해줌.(ex:onChange={getName})
-  
+  const dispatch = useDispatch() //useDispatch는 action을 던져준다.
+  const addContact=(event)=>{
+      event.preventDefault() //계속 리프레쉬 되는걸 막습니다. 
+      dispatch({type:'ADD_CONTATCT' , payload:{name, phoneNumber}}) //던져지는 action은 항상 type과 payload 키를 가지고 있다.
+                                                //payload:{name:name, phoneNumber:phoneNumber} 이름과 키값이 같다면 하나로 줄여도 된다.          
+    }                                                       
+
   return (
+    //Form은 onclick을 하면 안되고, onSubmit으로 줘야한다.
     <div>
-      <Form>
+      <Form onSubmit={addContact}>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>이름</Form.Label>
           <Form.Control 
--- noona-phonebook/src/redux/reducer/reducer.js
+++ noona-phonebook/src/redux/reducer/reducer.js
@@ -1,7 +1,39 @@
-let initialState={} //기본적인 세팅값이 없을 때는 공백으로 둔다.
+let initialState={
+    contactList:[] //이 배열안에 연락처들을 넣을 예정
+} //기본적인 세팅값이 없을 때는 공백으로 둔다.
 
 function reducer(state=initialState,action){ //state는 항상 처음에 initialState를 넣어줘서 초기화 해야됨
-
+    //.action값이 너무 많다면 안쓰게 하는 객체destructuring 문법!.action은 객채(type:,payload: 가 있는)다. 
+    const {type, payload} = action
+    switch(type){
+        case 'ADD_CONTACT':
+          return{
+            ...state,
+            contactList:[...state.contactList,
+                {
+                  name:payload.name,
+                  phoneNumber:payload.phoneNumber 
+                }
+            ]
+        }
+        default:  //switch문은 항상 default값을 써줘야 한다.
+            return {...state}
+    }
+    // .action다 쓴 코드
+    // switch(action.type){
+    //     case 'ADD_CONTACT':
+    //       return{
+    //         ...state,
+    //         contactList:[...state.contactList,
+    //             {
+    //               name:action.payload.name,
+    //               phoneNumber:action.payload.phoneNumber 
+    //             }
+    //         ]
+    //     }
+    //     default:  switch문은 항상 default값을 써줘야 한다.
+    //         return {...state} 
+    // }
 }
 
 export default reducer
