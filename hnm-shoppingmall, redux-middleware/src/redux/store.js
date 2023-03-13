5a275da4cbd7712d7c6434b1e8a2bd9f3869e2a8
choijongan <mmm7134@naver.com>
2023-03-13 22:06:43 +0900

    미들웨어 세팅완료

--- hnm-shoppingmall, redux-middleware/src/redux/store.js
+++ hnm-shoppingmall, redux-middleware/src/redux/store.js
@@ -1,5 +1,6 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import productReducer from "./reducers/ProductReducer";
-let store = createStore(productReducer)
+let store = createStore(productReducer, applyMiddleware(thunk))
 
 export default store
