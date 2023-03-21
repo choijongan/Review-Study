e70c1415bdde1ae387bbbd6b52a36fb8e4c1e602
choijongan <mmm7134@naver.com>
2023-03-21 21:53:16 +0900

    리덕스 데브툴스 사용해보기

--- hnm-shoppingmall, redux-middleware/src/redux/store.js
+++ hnm-shoppingmall, redux-middleware/src/redux/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import rootReducer from './reducers' //그냥 이렇게 써도되고, /reducers/index해도됨. 자동적용
-let store = createStore(rootReducer, applyMiddleware(thunk)) //임의로 두 reducer 합친 index이름을 rootReducer로 주고 store로 보낸다
+let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk))) //임의로 두 reducer 합친 index이름을 rootReducer로 주고 store로 보낸다
 
 export default store
