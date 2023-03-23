1add1b3375cd130161f12f03ff9d0d7c918df3cf
choijongan <mmm7134@naver.com>
2023-03-23 23:27:16 +0900

    createSlice 적용

--- hnm-shoppingmall, redux-middleware/src/redux/reducers/ProductReducer.js
+++ hnm-shoppingmall, redux-middleware/src/redux/reducers/ProductReducer.js
@@ -1,15 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 let initialState = {
     productList: [],
 }
 
-function productReducer(state = initialState, action){
-    let {type, payload} = action
-    switch (type) {
-        case 'GET_RPODUCT_SUCCESS':
-            return {...state, productList: payload.data}
-            default :
-                return {...state}
-    }
-}
+// function productReducer(state = initialState, action){
+//     let {type, payload} = action
+//     switch (type) {
+//         case 'GET_RPODUCT_SUCCESS':
+//             return {...state, productList: payload.data}
+//             default :
+//                 return {...state}
+//     }
+// }
 
-export default productReducer
+// export default productReducer
+createSlice({
+    name:'product',
+    initialState,
+    reducers:{
+        getAllproducts(state,action){
+           state.productList = action.payload.data  
+           //Slice가 알아서 return해주고, ...state해줌. 바꾸고 싶은 값만 멘션해주면 된다.
+           
+        }
+    }
+})
