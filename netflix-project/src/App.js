1ead40b8e58e166d0c537db8ebecc8d0368bad08
choijongan <mmm7134@naver.com>
2023-03-29 23:09:47 +0900

    라우터 세팅

--- netflix-project/src/App.js
+++ netflix-project/src/App.js
@@ -1,11 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import MovieDetail from './pages/MovieDetail';
+import Movies from './pages/Movies';
 //1. 3개 페이지 필요 홈페이지, movie페이지, movieDetail페이지
 //2. 홈페이지에서 배너를 볼 수 있다.
 //3. 3가지 섹션의 영화를 볼 수 있다. popular, top rated, upcoming
 //4. 각 영화에 마우스를 올려두면 제목, 장르, 점수, 연기도, 청불여보 볼 수 있다.
 //5. 영화를 슬라이드로 넘기면서 볼 수 있다,
 
-//6 영화 디테일 페이지에서 영화에 대한 디테일한 정보를 볼 수 있다. 포스터, 제목 줄거리 ,점수 등
+//6 영화 디테일 페이지에서 영화에 대한 디테일한 정보를 볼 수 있다. 포스터, 제목 줄거리, 점수 등
 //7. trailer를 누르면 trailer 볼 수 있다.
 //8. 영화에 리뷰도 볼 수 있다.
 //9. 관련된 영화도 볼 수 있다.
@@ -18,7 +22,11 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/movies' element={<Movies/>}/>
+          <Route path='/movies/:id' element={<MovieDetail/>}/>
+        </Routes>
     </div>
   );
 }
