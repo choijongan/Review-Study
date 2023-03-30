45e178797684f0d95f5627d189c8eb2eb39572be
choijongan <mmm7134@naver.com>
2023-03-30 21:38:30 +0900

    리덕스 기본세팅

---
+++ netflix-project/src/redux/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+//많은 reducer를 하나로 합치는 작업하는 곳(combineReducer)
+
+export default combineReducers({})
---
+++ netflix-project/src/redux/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+import rootReducer from './reducers'
+
+
+let store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
+
+export default store
