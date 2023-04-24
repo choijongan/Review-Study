41359349d84b87a27e22540a06083a06905c0e4e
choijongan <mmm7134@naver.com>
2023-04-24 20:13:58 +0900

    배너 API 이미지, 제목, 내용 불러오기

--- netflix-project/src/components/Banner.js
+++ netflix-project/src/components/Banner.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const Banner = ({movie}) => {
+  console.log('movie?',movie)
   return (
-    <div>Banner</div>
+    <div
+    className='banner' 
+    style={{
+      backgroundImage: 
+      'url('+ //url 무조건 소문자, ''로 감싸주고 + 넣어줘야됨(스트링으로 감싸기), ``다이나믹한 값으로 살려야되서 '' '' 따로묶음.   
+        `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${movie.poster_path}` +
+        ')'
+      }}
+      >
+        <div>
+          <h1>{movie.title}</h1>
+          <p>{movie.overview}</p>
+        </div>
+      </div>
   )
 }
 
