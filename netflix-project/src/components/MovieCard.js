89ae70d0802c6c594880883a757f5e74f5d5a4e7
choijongan <mmm7134@naver.com>
2023-05-10 19:54:15 +0900

    moviecard 이미지

--- netflix-project/src/components/MovieCard.js
+++ netflix-project/src/components/MovieCard.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const MovieCard = () => {
   return (
-    <div>MovieCard </div>
+    <div
+    className='card' 
+    style={{
+      backgroundImage:
+      "url('https://www.themoviedb.org/t/p/w355_and_h200_multi_faces/A7JQ7MIV5fkIxceI5hizRIe6DRJ.jpg')"
+    }}>MovieCard </div> //이미지는 항상 "" string타입 안에 넣기.
   )
 }
 
