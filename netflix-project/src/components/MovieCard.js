d56b35aa2de55b896ae5c27fa32652a1bd6432b3
choijongan <mmm7134@naver.com>
2023-05-10 19:58:56 +0900

    slide에 적용

--- netflix-project/src/components/MovieCard.js
+++ netflix-project/src/components/MovieCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieCard = () => {
   return (
     <div
-    className='card' 
+    className='moviecard' //card라는 이름은 이미있어서 적용이 되지않는다.
     style={{
       backgroundImage:
       "url('https://www.themoviedb.org/t/p/w355_and_h200_multi_faces/A7JQ7MIV5fkIxceI5hizRIe6DRJ.jpg')"
