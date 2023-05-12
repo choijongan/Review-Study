adb89d6dd6c8333efe01370dcfaf884fe5048958
choijongan <mmm7134@naver.com>
2023-05-12 18:24:40 +0900

    카드 css 정보 입력

--- netflix-project/src/components/MovieCard.js
+++ netflix-project/src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Badge } from 'react-bootstrap'
 
 const MovieCard = ({ item }) => {
   return (
@@ -8,9 +9,13 @@ const MovieCard = ({ item }) => {
       backgroundImage:
       "url("+`https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${item.poster_path}`+")"
     }}>
-        <div>
+        <div className='overlay'>
           <h1>{item.title}</h1>
-          <div>{item.genre_ids.map(id=>)}</div>
+          <div>{item.genre_ids.map((id)=><Badge bg="danger">{id}</Badge>)}</div>
+          <div>
+            <span>{item.vote_average}</span>
+            <span>{item.adult?'청소년 관람불가' : 'Under 18'}</span>
+          </div>
         </div> 
       </div> 
       //url 무조건 소문자, ''로 감싸주고 + 넣어줘야됨(스트링으로 감싸기), ``다이나믹한 값으로 살려야되서 '' '' 따로묶음.  
