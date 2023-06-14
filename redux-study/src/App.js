a7ad862320548a2c5caa22f829c5b42b530ad09b
choijongan <mmm7134@naver.com>
2023-06-14 19:12:18 +0900

    payload 수업

--- redux-study/src/App.js
+++ redux-study/src/App.js
@@ -5,18 +5,27 @@ import Box from './component/Box';
 
 
 function App() {
-  const count = useSelector(state=>state.count)  
+  let count = useSelector(state=>state.count)  
+
+  let id = useSelector(state=>state.id)
+
+  let password = useSelector(state=>state.password)
 
   const dispatch = useDispatch()
 
   const increase = () => {
-    dispatch({type:'INCREMENT'})
-    
+    dispatch({type:'INCREMENT', payload:{num:5}})
+  }
+
+  const login=()=>{
+    dispatch({type:'LOGIN',payload:{id:'jongan',password:'123'}})
   }
   return (
     <div>
+      <h1>{id},{password}</h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={login}>Login</button>
       <Box />
     </div>
   );
--- redux-study/src/redux/reducer/reducer.js
+++ redux-study/src/redux/reducer/reducer.js
@@ -1,18 +1,26 @@
 let initialState={
     count:0,
+    id:'',
+    password:'',
 }
 
 
 
 function reducer(state = initialState, action){
+    console.log('action은 뭘까?',action)
     // if(action.type === 'INCREMENT') {
     //     return{...state,count:state.count+1} 
     // }
     switch(action.type){
         case 'INCREMENT':
-            return {...state, count:state.count+1}
+            return {...state, count:state.count+action.payload.num}
+        case 'LOGIN':
+            return {...state, 
+                id:action.payload.id,
+                password:action.payload.password
+            }
         default:
-            return {...state, count:state.count+1}
+            return {...state}
     }
 
     
