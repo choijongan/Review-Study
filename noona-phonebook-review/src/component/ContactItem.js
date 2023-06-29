07d6ff91d3c2e9be14b87236ad13c21d19423abb
choijongan <mmm7134@naver.com>
2023-06-29 20:14:47 +0900

    연락처보여주기

--- noona-phonebook-review/src/component/ContactItem.js
+++ noona-phonebook-review/src/component/ContactItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 
-const ContactItem = () => {
+const ContactItem = ({item}) => {
   return (
     <Row>
         <Col lg={2}>
@@ -10,8 +10,8 @@ const ContactItem = () => {
             src='https://www.shutterstock.com/image-vector/unknown-person-vector-icon-flat-260nw-656925412.jpg' />
         </Col>
         <Col lg={10}>
-            <div>최종안</div>
-            <div>01012345678</div>
+            <div>{item.name}</div>
+            <div>{item.phoneNumber}</div>
         </Col>
     </Row>
   )
