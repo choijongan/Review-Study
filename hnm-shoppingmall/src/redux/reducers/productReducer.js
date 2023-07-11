5618131648cf3e86f74ed2147a5fbf425bc29d7c
choijongan <mmm7134@naver.com>
2023-07-11 23:25:54 +0900

    redux toolkit 슬라이스

--- hnm-shoppingmall/src/redux/reducers/productReducer.js
+++ hnm-shoppingmall/src/redux/reducers/productReducer.js
@@ -1,15 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit"
 let initialState = {
     productList:[]
 }
 
-function ProductReducer(state=initialState,action){
-    let {type, payload} = action
-    switch(type){
-        case 'GET_PRODUCT_SUCCESS':
-            return{...state, productList:payload.data}
-        default:
-            return{...state}
-    }
-}
+// function ProductReducer(state=initialState,action){
+//     let {type, payload} = action
+//     switch(type){
+//         case 'GET_PRODUCT_SUCCESS':
+//             return{...state, productList:payload.data}
+//         default:
+//             return{...state}
+//     }
+// }
 
-export default ProductReducer;
+// export default ProductReducer;
+
+
+const productSlice =  createSlice({
+    name:'product',
+    initialState,
+    reducers:{
+        getAllProducts(state,action){
+            state.productList = action.payload.data
+        },
+        getSingleProduct(state,action){
+            state.selectedItem = action.payload.data
+        }
+    }
+})
+
+console.log('pppp',productSlice)
